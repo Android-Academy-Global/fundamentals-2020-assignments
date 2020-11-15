11243dd296656004debcc8846d670d67b3e36804
Sergio <webanimal.group@gmail.com>
2020-11-15 16:29:06 +0300

    WS1: Checked screen rotation part.

--- app/src/main/java/com/android/fundamentals/task_one/done/WS1DoneMainActivity.kt
+++ app/src/main/java/com/android/fundamentals/task_one/done/WS1DoneMainActivity.kt
@@ -1,6 +1,7 @@
 package com.android.fundamentals.task_one.done
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,39 +9,67 @@ import com.android.fundamentals.R
 
 class WS1DoneMainActivity : AppCompatActivity() {
 
-    private var count = 0
+    private var counter = 0
 
     private var tvValue: TextView? = null
     private var btnIncrementer: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "TEST::onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ws1)
 
-        tvValue = findViewById<TextView>(R.id.tv_value).apply {
-            text = "The value is: $count"
+        setupUi()
+        savedInstanceState.ifNull {
+            updateData(counter)
         }
-        btnIncrementer = findViewById<Button>(R.id.btn_incrementer).apply {
-            setOnClickListener {
-                count++
-                tvValue?.text = "The value is: $count"
-            }
-        }
-
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "TEST::onSaveInstanceState")
         super.onSaveInstanceState(outState)
-        outState.putInt(COUNT_ARGUMENT, count)
+
+        outState.putInt(COUNT_ARGUMENT, counter)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "TEST::onRestoreInstanceState")
         super.onRestoreInstanceState(savedInstanceState)
-        count = savedInstanceState.getInt(COUNT_ARGUMENT)
-        tvValue?.text = "The value is: $count"
+
+        counter = savedInstanceState.getInt(COUNT_ARGUMENT) // getInt() provides "0" by default
+        updateData(counter)
     }
 
-    companion object {
-        private const val COUNT_ARGUMENT = "CountArgument"
+    override fun onDestroy() {
+        Log.d(TAG, "TEST::onDestroy")
+
+        tvValue = null
+        btnIncrementer = null
+
+        super.onDestroy()
+    }
+
+    private fun setupUi() {
+        tvValue = findViewById(R.id.tv_value)
+
+        btnIncrementer = findViewById<Button>(R.id.btn_incrementer).apply {
+            setOnClickListener {
+                counter++
+                updateData(counter)
+            }
+        }
+    }
+
+    private fun updateData(counter: Int) {
+        tvValue?.text = "The value is: $counter"
     }
 }
+
+private fun <T> T?.ifNull(block: () -> Unit) {
+    if (this == null) {
+        block()
+    }
+}
+
+private const val COUNT_ARGUMENT = "CountArgument"
+private const val TAG = "WS1DoneMainActivity"
--- app/src/main/java/com/android/fundamentals/task_one/task/WS1TaskMainActivity.kt
+++ app/src/main/java/com/android/fundamentals/task_one/task/WS1TaskMainActivity.kt
@@ -28,5 +28,4 @@ class WS1TaskMainActivity : AppCompatActivity() {
             }
         }
     }
-
 }
