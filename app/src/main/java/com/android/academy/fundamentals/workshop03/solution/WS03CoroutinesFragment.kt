3a9549cc671f0951b35dd841a82a0c963b18cdd5
Eugen Martynov <eugenemartynov@gmail.com>
2020-11-30 23:32:16 +0100

    Add start and stop buttons

--- app/src/main/java/com/android/academy/fundamentals/workshop03/solution/WS03CoroutinesFragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/workshop03/solution/WS03CoroutinesFragment.kt
@@ -1,6 +1,16 @@
 package com.android.academy.fundamentals.workshop03.solution
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.android.academy.fundamentals.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
-class WS03CoroutinesFragment : Fragment(R.layout.fragment_coroutines_scope_cancel)
+class WS03CoroutinesFragment : Fragment(R.layout.fragment_coroutines_scope_cancel) {
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
