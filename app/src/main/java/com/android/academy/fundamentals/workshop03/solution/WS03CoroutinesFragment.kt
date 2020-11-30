b378b2e4cce92a5a02c3f18bc81f97d97abbbad6
Eugen Martynov <eugenemartynov@gmail.com>
2020-11-30 23:13:23 +0100

    Add solution fragment

---
+++ app/src/main/java/com/android/academy/fundamentals/workshop03/solution/WS03CoroutinesFragment.kt
@@ -0,0 +1,6 @@
+package com.android.academy.fundamentals.workshop03.solution
+
+import androidx.fragment.app.Fragment
+import com.android.academy.fundamentals.R
+
+class WS03CoroutinesFragment : Fragment(R.layout.fragment_coroutines_scope_cancel)
