21ab469af2c4927e0b8ca6786dedb8800d082e2f
Eugen Martynov <eugenemartynov@gmail.com>
2020-12-08 08:57:30 +0100

    Add static fragment

---
+++ app/src/main/java/com/android/academy/fundamentals/extra/StaticCoroutinesFragment.kt
@@ -0,0 +1,6 @@
+package com.android.academy.fundamentals.extra
+
+import androidx.fragment.app.Fragment
+import com.android.academy.fundamentals.R
+
+class StaticCoroutinesFragment : Fragment(R.layout.fragment_static)
