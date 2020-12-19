c3500609743f8b7eb98be5c33f3ec98757bd5b23
Sergio <webanimal.group@gmail.com>
2020-12-19 17:38:51 +0300

    Workshop 01-03. Api-key added.

--- app/src/main/java/com/android/academy/fundamentals/BaseFragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/BaseFragment.kt
@@ -2,13 +2,20 @@ package com.android.academy.fundamentals
 
 import android.util.Log
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 open class BaseFragment : Fragment() {
+	
+	var coroutineScope = createCoroutineScope()
 	
 	fun getBaseUrl(): String = BuildConfig.BASE_URL
 	
 	fun getApiKey(): String =
 		(context?.applicationContext as? KeyStore)?.getApiKey() ?: logInvalidApiKey()
+	
+	fun createCoroutineScope() = CoroutineScope(Job() + Dispatchers.IO)
 	
 	private fun logInvalidApiKey(): String {
 		Log.e("API:", "", IllegalArgumentException("Invalid api key error"))
--- app/src/main/java/com/android/academy/fundamentals/ws01/WS01Fragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/ws01/WS01Fragment.kt
@@ -1,13 +1,22 @@
 package com.android.academy.fundamentals.ws01
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.academy.fundamentals.BaseFragment
 import com.android.academy.fundamentals.R
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class WS01Fragment : BaseFragment() {
+	
+	private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+		Log.e(TAG, "Coroutine exception, scope active:${coroutineScope.isActive}", throwable)
+		coroutineScope = createCoroutineScope()
+	}
 	
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -18,7 +27,20 @@ class WS01Fragment : BaseFragment() {
 		return inflater.inflate(R.layout.fragment_ws01, container, false)
 	}
 	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		loadImage()
+	}
+	
+	private fun loadImage() {
+		coroutineScope.launch(exceptionHandler) {
+		}
+	}
+	
 	companion object {
+		private val TAG = WS01Fragment::class.java.simpleName
+		
 		fun create() = WS01Fragment()
 	}
 }
