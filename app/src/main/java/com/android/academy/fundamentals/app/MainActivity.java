29aa99fb1b8de8547ec05c773ca894eb8465863b
Yurii Anisimov <31529762+an-yurii@users.noreply.github.com>
2021-01-20 11:01:28 +0700

    project is initialised

---
+++ app/src/main/java/com/android/academy/fundamentals/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.android.academy.fundamentals.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
