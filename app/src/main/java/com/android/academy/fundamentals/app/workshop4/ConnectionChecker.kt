9a483a8ab4b63a73cd00bf6038f0d740b4985bc7
Konovalov Kirill <51828487+konovalov-k@users.noreply.github.com>
2021-01-23 09:03:38 +0300

    ex-9 w4: Add solution for WorkManager workshop

---
+++ app/src/main/java/com/android/academy/fundamentals/app/workshop4/ConnectionChecker.kt
@@ -0,0 +1,20 @@
+package com.android.academy.fundamentals.app.workshop4
+
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+
+object ConnectionChecker {
+    // TCP/HTTP/DNS (depending on the port, 53=DNS, 80=HTTP, etc.)
+    fun isOnline(): Boolean {
+        return try {
+            Socket().run {
+                connect(InetSocketAddress("8.8.8.8", 53), 500)
+                close()
+            }
+            true
+        } catch (e: IOException) {
+            false
+        }
+    }
+}
---
+++ app/src/main/java/com/android/academy/fundamentals/app/workshop4/solution/WS04FragmentSolution.kt
@@ -0,0 +1,21 @@
+package com.android.academy.fundamentals.app.workshop4.solution
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.work.WorkManager
+import com.android.academy.fundamentals.app.R
+
+class WS04FragmentSolution : Fragment(R.layout.fragment_ws04) {
+    private val workRepository = WorkRepositorySolution()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //NotTodo 4.4: Enqueue simpleRequest with WorkManager
+        //WorkManager.getInstance(requireContext()).enqueue(workRepository.simpleRequest)
+        //NotTodo 4.6: Enqueue delayedRequest with WorkManager
+        //WorkManager.getInstance(requireContext()).enqueue(workRepository.delayedRequest)
+        //NotTodo 4.8: Enqueue constrainedRequest with WorkManager
+        WorkManager.getInstance(requireContext()).enqueue(workRepository.constrainedRequest)
+    }
+}
