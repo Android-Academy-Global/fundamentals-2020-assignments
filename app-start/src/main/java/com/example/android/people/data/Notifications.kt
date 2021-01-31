eb39a3134bfcc0c3c641ec88e45170f400579d84
Eugen Martynov <eugenemartynov@gmail.com>
2021-01-31 16:41:13 +0100

    Prepare three TODO for the notification workshop

--- app-start/src/main/java/com/example/android/people/data/Notifications.kt
+++ app-start/src/main/java/com/example/android/people/data/Notifications.kt
@@ -16,17 +16,9 @@
 package com.example.android.people.data
 
 import android.app.Notification
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import androidx.annotation.WorkerThread
-import androidx.core.app.*
-import androidx.core.app.NotificationManagerCompat.IMPORTANCE_HIGH
-import androidx.core.graphics.drawable.IconCompat
-import androidx.core.net.toUri
-import com.example.android.people.MainActivity
-import com.example.android.people.R
-import com.example.android.people.ReplyReceiver
+import androidx.core.app.NotificationManagerCompat
 
 /**
  * Handles all operations related to [Notification].
@@ -46,6 +38,8 @@ class AndroidNotifications(private val context: Context) : Notifications {
         private const val CHANNEL_NEW_MESSAGES = "new_messages"
 
         private const val REQUEST_CONTENT = 1
+
+        private const val CHAT_TAG = "chat"
     }
 
     private val notificationManagerCompat: NotificationManagerCompat =
@@ -53,91 +47,61 @@ class AndroidNotifications(private val context: Context) : Notifications {
 
     override fun initialize() {
         if (notificationManagerCompat.getNotificationChannel(CHANNEL_NEW_MESSAGES) == null) {
-            notificationManagerCompat.createNotificationChannel(
-                NotificationChannelCompat.Builder(CHANNEL_NEW_MESSAGES, IMPORTANCE_HIGH)
-                    .setName(context.getString(R.string.channel_new_messages))
-                    .setDescription(context.getString(R.string.channel_new_messages_description))
-                    .build()
+            TODO(
+                """
+                
+                #1 Create The Notification Channel
+                 
+                a) Create `NotificationChannelCompat.Builder`
+                   with `CHANNEL_NEW_MESSAGES` as channel ID and `IMPORTANCE_HIGH` as 
+                        a channel importance
+                   use `R.string.channel_new_messages` as channel name
+                   use `R.string.channel_new_messages_description` as channel description
+                 
+                b) Pass built `NotificationChannelCompat` to the 
+                     `notificationManagerCompat.createNotificationChannel`
+                     
+                Note: Don't forget we have context in the class already
+                
+                """.trimIndent()
             )
         }
     }
 
     @WorkerThread
     override fun showNotification(chat: Chat) {
-        val icon = IconCompat.createWithContentUri(chat.contact.iconUri)
-        val contentUri = "https://android.example.com/chat/${chat.contact.id}".toUri()
-        val person = Person.Builder()
-            .setName(chat.contact.name)
-            .setIcon(icon)
-            .build()
-
-        val builder = NotificationCompat.Builder(context, CHANNEL_NEW_MESSAGES)
-            .setGroup(chat.contact.id.toString())
-            .setContentTitle(chat.contact.name)
-            .setContentText(chat.messages.last().text)
-            .setSmallIcon(R.drawable.ic_message)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setOnlyAlertOnce(true)
-            .setContentIntent(
-                PendingIntent.getActivity(
-                    context,
-                    REQUEST_CONTENT,
-                    Intent(context, MainActivity::class.java)
-                        .setAction(Intent.ACTION_VIEW)
-                        .setData(contentUri),
-                    PendingIntent.FLAG_UPDATE_CURRENT
-                )
-            )
-            // Direct Reply
-            .addAction(
-                NotificationCompat.Action
-                    .Builder(
-                        IconCompat.createWithResource(context, R.drawable.ic_send),
-                        context.getString(R.string.label_reply),
-                        PendingIntent.getBroadcast(
-                            context,
-                            REQUEST_CONTENT,
-                            Intent(context, ReplyReceiver::class.java).setData(contentUri),
-                            PendingIntent.FLAG_UPDATE_CURRENT
-                        )
-                    )
-                    .addRemoteInput(
-                        RemoteInput.Builder(ReplyReceiver.KEY_TEXT_REPLY)
-                            .setLabel(context.getString(R.string.hint_input))
-                            .build()
-                    )
-                    .setAllowGeneratedReplies(true)
-                    .build()
-            )
-            .setStyle(
-                NotificationCompat.MessagingStyle(person)
-                    .run {
-                        for (message in chat.messages) {
-                            val m = NotificationCompat.MessagingStyle.Message(
-                                message.text,
-                                message.timestamp,
-                                if (message.isIncoming) person else null
-                            ).apply {
-                                if (message.photoUri != null) {
-                                    setData(message.photoMimeType, message.photoUri)
-                                }
-                            }
-                            if (!message.isNew) {
-                                addHistoricMessage(m)
-                            } else {
-                                addMessage(m)
-                            }
-                        }
-                        this
-                    }
-                    .setGroupConversation(false)
-            )
-        .setWhen(chat.messages.last().timestamp)
-
-        notificationManagerCompat.notify("chat", chat.contact.id.toInt(), builder.build())
+        TODO(
+            """
+                
+                #2 Show The Notification
+                
+                a) Create `NotificationCompat.Builder`
+                   with `CHANNEL_NEW_MESSAGES` as channel ID
+                   use `chat.contact.name` as Group
+                   use `chat.contact.name` as Content Title
+                   use last message text from chat.messages as Content Text
+                   use `R.drawable.ic_message` as Small Icon
+                   use `IMPORTANCE_HIGH` as Priority
+                   use last message timestamp as When
+                   
+                b) Pass built notification to `notificationManagerCompat.notify`
+                   use `CHAT_TAG` as notification Tag
+                   use `chat.contact.id` as notification ID
+                   
+            """.trimIndent()
+        )
     }
 
     override fun dismissNotification(chatId: Long) {
-        notificationManagerCompat.cancel("chat", chatId.toInt())
+        TODO(
+            """
+                
+                #3 Cancel Chat Notification(s)
+                
+                Call `notificationManagerCompat.cancel` with `CHAT_TAG` as tag and `chatId` as 
+                    notification ID
+                    
+            """.trimIndent()
+        )
     }
 }
