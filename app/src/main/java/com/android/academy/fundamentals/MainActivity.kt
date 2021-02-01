2fafcf8ce6555bd3c3ea1c0f20e9ed45e802bad7
Maria_Kirdun <Maria_Kirdun@epam.com>
2021-02-01 22:52:22 +0300

    add fragment to main activity

--- app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .addToBackStack(null)
+//                .replace(R.id.fragment_container_view, Fragment())
+                .commit()
+        }
     }
 }
