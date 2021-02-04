fa0d72c6dee9bb0c08cc6e5ebccfddeaf10d77a5
Liudmila Budanitskaya <liudmila28march1991@gmail.com>
2021-02-04 16:12:21 +0300

    view and viewgroup assignment 3

--- app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_w2)
+        setContentView(R.layout.layout_w3)
     }
 }
