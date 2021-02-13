ef42fc522c756e53c4d18c9b5fce349c15c86270
k.zagulin <k.zagulin@tinkoff.ru>
2021-02-13 03:25:12 +0300

    changed todo text

--- app/src/main/java/com/android/academy/fundamentals/WS02Fragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/WS02Fragment.kt
@@ -32,52 +32,52 @@ class WS02Fragment : Fragment(R.layout.fragment_ws02) {
 
     private fun startAnimations() {
         //TODO 01: Inflate animator from R.animator.prepare_rocket.
-        // Call static function loadAnimator of AnimatorInflater to inflate animator from XML.
+        // Call static method <loadAnimator> of AnimatorInflater to inflate animator from XML.
         // Assign it to rocketPrepareAnimator local variable.
-        // Set rocket as animator target. Use setTarget method.
+        // Set rocket as animator target. Use <setTarget> method.
 
-        //TODO 02: Inflate animator from R.animator.fire  in the same way
+        //TODO 02: Inflate animator from R.animator.fire in the same way
         // and assign it to fireAnimator local variable
 
-        //TODO 03: Call playTogether method of rocketPrepareAndFireStartAnimatorSet
-        // and pass rocketPrepareAnimator and  fireAnimator to arguments
+        //TODO 03: Call <playTogether> method of rocketPrepareAndFireStartAnimatorSet
+        // and pass rocketPrepareAnimator and fireAnimator to arguments
 
-        //TODO 04 Add listener to rocketPrepareAndFireAnimatorSet - use addListener method
-        // We need only onEnd method. We will continue write code inside this method.
-        // Uncomment code block (49-71 line numbers)
+        //TODO 04 Uncomment rocketPrepareAndFireAnimatorSet listener
+        // We have already added listener
+        // Just uncomment code block (49-71 line numbers)
 
-        rocketPrepareAndFireAnimatorSet.addListener(onEnd = {
+//        rocketPrepareAndFireAnimatorSet.addListener(onEnd = {
+//
+//            //TODO 05: Create ViewPropertyAnimator by calling <animate> method of rocket view.
+//            // Use <x> method to set 0F
+//            // Use <y> method to set 0F
+//            // Use <setDuration> method to set 5000
+//            // Call <start> method to start animation
+//
+//            //TODO 06: Create ViewPropertyAnimator by calling <animate> method of fire view.
+//            // Use <x> method to set (rocket.width - fire.width) / 2F
+//            // Use <y> method to set rocket.height.toFloat()
+//            // Use <setDuration> method to set 5000
+//            // Call <start> method to start animation
+//
+//            //TODO 07: Create ViewPropertyAnimator by calling <animate> method of rocket view.
+//            // Use <alpha> method to set 0F
+//            // Use <setDuration> method to set 1000
+//            // Use <setStartDelay> method to set 4000
+//            // Call <start> method to start animation
+//
+//            //TODO 08: repeat TODO 08 for fire
+//
+//        })
 
-            //TODO 05: Create ViewPropertyAnimator by calling animate method of rocket view.
-            // Use x method to set 0f
-            // Use y method to set 0f
-            // Use setDuration method to set 5000
-            // Call start method to start animation
-
-            //TODO 06: Create ViewPropertyAnimator by calling animate method of fire view.
-            // Use x method to set (rocket.width - fire.width) / 2f
-            // Use y method to set rocket.height.toFloat()
-            // Use setDuration method to set 5000
-            // Call start method to start animation
-
-            //TODO 07: Create ViewPropertyAnimator by calling animate method of rocket view.
-            // Use alpha method to set 0f
-            // Use setDuration method to set 1000
-            // Use setStartDelay method to set 4000
-            // Call start method to start animation
-
-            //TODO 08: repeat TODO 08 for fire
-
-        })
-
-        //TODO 09: start rocketPrepareAndFireAnimatorSet
+        //TODO 09: Call <start> method of rocketPrepareAndFireAnimatorSet
 
     }
 
     override fun onDestroy() {
         //TODO 10: stop all animations
-        // call removeAllListeners and cancel methods of rocketPrepareAndFireAnimatorSet
-        // call clearAnimation of rocket and fire views
+        // call <removeAllListeners> and <cancel> methods of rocketPrepareAndFireAnimatorSet
+        // call <clearAnimation> of rocket and fire views
 
         super.onDestroy()
     }
