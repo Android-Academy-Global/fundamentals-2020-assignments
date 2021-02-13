848dc326b07e9ca59618013e4f83ee81b60321cd
Maria_Kirdun <Maria_Kirdun@epam.com>
2021-02-13 19:15:22 +0300

    animations for recycler view

--- app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
@@ -2,8 +2,7 @@ package com.android.academy.fundamentals
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.android.academy.fundamentals.extra.StaticCoroutinesFragment
+import com.android.academy.fundamentals.workshop03.solution.WS03SolutionFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .addToBackStack(null)
-                .replace(R.id.fragment_container_view, Fragment())
+                .replace(R.id.fragment_container_view, WS03SolutionFragment())
                 .commit()
         }
     }
