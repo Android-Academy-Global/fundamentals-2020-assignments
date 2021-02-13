1ab0ee4b4885dcef12d3741b8894f895cbd40bfc
«Timurovic» <ttimurovic@gmail.com>
2021-02-13 23:18:45 +0300

    deleted addToBackStack

--- app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.academy.fundamentals.workshops.WS01Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .addToBackStack(null)
                 .replace(R.id.fragment_container_view, WS01Fragment())
                 .commit()
         }
